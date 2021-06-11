68f2735c927166882a81475986b2ca67b1f9c16c
Jeffrey Baker <jeffrey1871@gmail.com>
2021-06-10 20:32:13 -0500

    Section 1

---
+++ BestBuyASPNET/ProductRepo.cs
@@ -0,0 +1,30 @@
+﻿using BestBuyASPNET.Models;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BestBuyASPNET
+{
+    public class ProductRepo : IProductRepo
+    {
+        private readonly IDbConnection _conn;
+
+        public ProductRepo(IDbConnection conn)
+        {
+            _conn = conn;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("SELECT * FROM PRODUCTS;");
+        }
+
+        IEnumerable<Product> IProductRepo.GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
