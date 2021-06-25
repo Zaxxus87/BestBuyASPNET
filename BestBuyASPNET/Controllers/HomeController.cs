bb6f258896a05da275c19b77794aaa6e4d4170f8
Jeffrey Baker <jeffrey1871@gmail.com>
2021-06-25 18:57:12 -0500

    Published 3.0

--- BestBuyASPNET/Controllers/HomeController.cs
+++ BestBuyASPNET/Controllers/HomeController.cs
@@ -22,7 +22,11 @@ namespace BestBuyASPNET.Controllers
         {
             return View();
         }
-
+        
+        public IActionResult Privacy()
+        {
+            return View();
+        }
         
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
